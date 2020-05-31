d9c058a9b4f58b1ccf8a6c148f22e91afa14cec3
ML <marvinleiers@gmail.com>
2020-05-31 10:09:16 +0200

    Initial commit

---
+++ src/main/java/de/marvin2k0/skywars/Skywars.java
@@ -0,0 +1,12 @@
+package de.marvin2k0.skywars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Skywars extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        
+    }
+}
