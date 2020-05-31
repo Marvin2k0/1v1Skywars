f4d9360c6671a66274afcdb60ce1f84c08f3e217
ML <marvinleiers@gmail.com>
2020-05-31 13:11:52 +0200

    Lobby erstellt

--- src/main/java/de/marvin2k0/skywars/listener/GameListener.java
+++ src/main/java/de/marvin2k0/skywars/listener/GameListener.java
@@ -5,11 +5,13 @@ import de.marvin2k0.skywars.utils.Locations;
 import de.marvin2k0.skywars.utils.Text;
 import de.marvinleiers.minigameapi.events.GameStartEvent;
 import de.marvinleiers.minigameapi.events.PlayerGameJoinEvent;
+import de.marvinleiers.minigameapi.events.PlayerInLobbyItemInteractEvent;
 import de.marvinleiers.minigameapi.game.Game;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 
 public class GameListener implements Listener
 {
@@ -43,5 +45,14 @@ public class GameListener implements Listener
 
         for (Player player : game.getPlayers())
             player.teleport(spawn);
+    }
+
+    @EventHandler
+    public void onLobbyItem(PlayerInLobbyItemInteractEvent event)
+    {
+        ItemStack item = event.getItem();
+
+        if (item.hasItemMeta() && item.getItemMeta().getDisplayName().equals(Text.get("bedname", false)))
+            event.getGame().leave(event.getPlayer());
     }
 }
